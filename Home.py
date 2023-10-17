f2ef7a3e512cfba49843878cd5a64dde8ef1865b
wayneii <95460389+wayneii@users.noreply.github.com>
2023-10-17 21:07:42 +0000

    Your commit message

--- Home.py
+++ Home.py
@@ -4,6 +4,22 @@ import importlib
 import sys
 import os
 
+# Create a banner using Markdown with HTML/CSS styling
+st.markdown("""
+<style>
+.banner {
+  background-color: black;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
+}
+</style>
+
+<div class="banner">
+  <h1 style="color:white;">Overview of VA Recommendations</h1>
+</div>
+""", unsafe_allow_html=True)
+
 # Initialize session state for the main (entrypoint) page
 if 'current_page' not in st.session_state:
     st.session_state['current_page'] = 'Home'  # This sets 'Home' as the default page
@@ -18,18 +34,63 @@ pages += [f[:-3] for f in os.listdir(pages_dir) if f.endswith('.py')]
 # Sort the pages to control the order in which they appear
 pages.sort()
 
+#### THIS IS WHERE WE PUT THE CODE TO CHANGE THE SIDEBAR
+#### e.g. filter, year selection, or 
 # Create the page selection box in the sidebar
-page_selection = st.sidebar.selectbox("Navigate to:", pages)
-st.session_state['current_page'] = page_selection
+#page_selection = st.sidebar.selectbox("Navigate to:", pages)
+#st.session_state['current_page'] = page_selection
 
 # Home page content
-if st.session_state['current_page'] == 'Home':
-    st.title("Welcome to My App")
-    st.write("This is the Home page.")
+#if st.session_state['current_page'] == 'Home':
+    #st.title("Welcome to My App")
+    #st.write("This is the Home page.")
     # Add any other content and functionality you want on the Home page here
 
 # Dynamic import and display of other pages
-else:
-    page_module = importlib.import_module(f'pages.{page_selection}')
-    page_module.show()
+#else:
+    #page_module = importlib.import_module(f'pages.{page_selection}')
+    #page_module.show()
+
+
+# Display of Themes
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+st.title("Topic Distribution Pie Chart by Year")
+
+# Dummy data: nested dictionary where the outer key is the year, and the inner dictionary contains topic sizes.
+data = {
+    2014: {'Healthcare Innovations': 10, 'Sexual Assault': 20, 'Homelessness': 25, 'Business Transformation': 10, 'Controversial Topics': 5, 'Data-Driven Outreach': 30},
+    2016: {'Healthcare Innovations': 15, 'Sexual Assault': 35, 'Homelessness': 20, 'Business Transformation': 10, 'Controversial Topics': 10, 'Data-Driven Outreach': 10},
+    2018: {'Healthcare Innovations': 20, 'Sexual Assault': 15, 'Homelessness': 25, 'Business Transformation': 20, 'Controversial Topics': 10, 'Data-Driven Outreach': 10},
+    2020: {'Healthcare Innovations': 25, 'Sexual Assault': 30, 'Homelessness': 20, 'Business Transformation': 5,  'Controversial Topics': 10, 'Data-Driven Outreach': 10},
+    2022: {'Healthcare Innovations': 30, 'Sexual Assault': 25, 'Homelessness': 15, 'Business Transformation': 15, 'Controversial Topics': 5,  'Data-Driven Outreach': 10},
+}
+
+# Sidebar: Year selection
+selected_year = st.sidebar.slider('Select a year range', min_value=min(data.keys()), max_value=max(data.keys()), value=(min(data.keys()), max(data.keys())))
+
+# Filter data based on selected years
+filtered_data = {year: sizes for year, sizes in data.items() if year >= selected_year[0] and year <= selected_year[1]}
+
+# Aggregate data across the selected years
+aggregated_data = {}
+for sizes in filtered_data.values():
+    for topic, size in sizes.items():
+        aggregated_data[topic] = aggregated_data.get(topic, 0) + size
+
+labels = list(aggregated_data.keys())
+sizes = list(aggregated_data.values())
+
+# Pie chart settings
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'purple', 'orange']
+explode = (0, 0.1, 0, 0, 0, 0)  # explode the 2nd slice (Sexual Assault)
+
+plt.figure(figsize=(10, 6))
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+
+# Display the pie chart
+st.pyplot(plt)
 
--- pages/onthetable.py
+++ pages/onthetable.py
@@ -1,5 +1,44 @@
 import streamlit as st
+import pandas as pd
 
-def show():
-    st.write("You're viewing the On the Table content!")
-    # Add your content and functionality for the "Page_two" page here
+# Sample data
+data = {
+    'ID': [1, 2, 3, 4, 5, 6, 7],
+    'Year': [1996, 2000, 2005, 2010, 2015, 2020, 2022],
+    'Short_Recommendation': ['Improve Security', 'Enhance Security', 'Boost Security', 'Upgrade Security', 'Strengthen Security', 'Improve Security Again', 'Strengthen Security Again'],
+    'Full_Recommendation': ['Full text for Improve Security', 'Full text for Enhance Security', 'Full text for Boost Security', 'Full text for Upgrade Security', 'Full text for Strengthen Security', 'Full text for Improve Security Again', 'Full text for Strengthen Security Again'],
+    'Similar_to': ['None', '1', '2', '3', '4', '5', '6'],
+    'Status': ['Not Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred'],
+    'Category': ['Security', 'Security', 'Security', 'IT', 'IT', 'HR', 'HR']
+}
+df = pd.DataFrame(data)
+
+st.title('Recommendations on the Table')
+
+# Create a category filter with counts
+categories = df['Category'].unique()
+category_counts = df[df['Status'] == 'Not Concurred'].groupby('Category').size()
+category_options = [f"{category} ({category_counts.get(category, 0)})" for category in categories]
+
+# Use multiselect instead of dropdown
+selected_categories = st.sidebar.multiselect('Filter by Category:', category_options)
+selected_categories = [cat.split(' ')[0] for cat in selected_categories]
+
+# Filter the DataFrame based on the status 'Not Concurred' and selected categories
+if selected_categories:
+    df_filtered = df[(df['Status'] == 'Not Concurred') & (df['Category'].isin(selected_categories))]
+else:
+    df_filtered = df[df['Status'] == 'Not Concurred']
+
+# Loop through filtered DataFrame and display interactive elements
+for index, row in df_filtered.iterrows():
+    button_label = f"Recommendation {row['ID']} from {row['Year']} (Status: {row['Status']})"
+    if st.button(button_label):
+        st.write(f"**Full Recommendation**: {row['Full_Recommendation']}")
+        
+        similar_to_ids = row['Similar_to'].split(',')
+        similar_recs = df[df['ID'].astype(str).isin(similar_to_ids)]
+        
+        st.write("**Similar to the following recommendations:**")
+        for sim_index, sim_row in similar_recs.iterrows():
+            st.write(f"Recommendation {sim_row['ID']} from {sim_row['Year']}: {sim_row['Full_Recommendation']}")
