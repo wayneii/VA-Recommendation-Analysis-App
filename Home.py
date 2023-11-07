1615813eb4a7f8290144506e236f886d1a60dc11
wayneii <95460389+wayneii@users.noreply.github.com>
2023-11-07 22:59:18 +0000

    v0.2 of the app

--- Home.py
+++ Home.py
@@ -3,7 +3,16 @@ import streamlit as st
 import importlib
 import sys
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Read the data
+df = pd.read_excel('/workspaces/VA-Recommendation-Analysis-App/ACWV Recommendations with Categories and Similarity Groups.xlsx')
+
+# Make the year column an integer
+df['Year'] = df['Year'].astype(int)
+
+################################## BANNER ######################################################################
 # Create a banner using Markdown with HTML/CSS styling
 st.markdown("""
 <style>
@@ -16,9 +25,11 @@ st.markdown("""
 </style>
 
 <div class="banner">
-  <h1 style="color:white;">Overview of VA Recommendations</h1>
+  <h1 style="color:white;">ACWV Recommendations</h1>
 </div>
 """, unsafe_allow_html=True)
+
+################################## PAGES TAB ##################################################################
 
 # Initialize session state for the main (entrypoint) page
 if 'current_page' not in st.session_state:
@@ -34,63 +45,127 @@ pages += [f[:-3] for f in os.listdir(pages_dir) if f.endswith('.py')]
 # Sort the pages to control the order in which they appear
 pages.sort()
 
-#### THIS IS WHERE WE PUT THE CODE TO CHANGE THE SIDEBAR
-#### e.g. filter, year selection, or 
-# Create the page selection box in the sidebar
-#page_selection = st.sidebar.selectbox("Navigate to:", pages)
-#st.session_state['current_page'] = page_selection
 
-# Home page content
-#if st.session_state['current_page'] == 'Home':
-    #st.title("Welcome to My App")
-    #st.write("This is the Home page.")
-    # Add any other content and functionality you want on the Home page here
-
-# Dynamic import and display of other pages
-#else:
-    #page_module = importlib.import_module(f'pages.{page_selection}')
-    #page_module.show()
-
-
-# Display of Themes
-import streamlit as st
-import matplotlib.pyplot as plt
-
-
-st.title("Topic Distribution Pie Chart by Year")
-
-# Dummy data: nested dictionary where the outer key is the year, and the inner dictionary contains topic sizes.
-data = {
-    2014: {'Healthcare Innovations': 10, 'Sexual Assault': 20, 'Homelessness': 25, 'Business Transformation': 10, 'Controversial Topics': 5, 'Data-Driven Outreach': 30},
-    2016: {'Healthcare Innovations': 15, 'Sexual Assault': 35, 'Homelessness': 20, 'Business Transformation': 10, 'Controversial Topics': 10, 'Data-Driven Outreach': 10},
-    2018: {'Healthcare Innovations': 20, 'Sexual Assault': 15, 'Homelessness': 25, 'Business Transformation': 20, 'Controversial Topics': 10, 'Data-Driven Outreach': 10},
-    2020: {'Healthcare Innovations': 25, 'Sexual Assault': 30, 'Homelessness': 20, 'Business Transformation': 5,  'Controversial Topics': 10, 'Data-Driven Outreach': 10},
-    2022: {'Healthcare Innovations': 30, 'Sexual Assault': 25, 'Homelessness': 15, 'Business Transformation': 15, 'Controversial Topics': 5,  'Data-Driven Outreach': 10},
-}
+################################ FILTER DATA ####################################################################
 
 # Sidebar: Year selection
-selected_year = st.sidebar.slider('Select a year range', min_value=min(data.keys()), max_value=max(data.keys()), value=(min(data.keys()), max(data.keys())))
+elected_year = st.sidebar.slider('Select a year range', min_value=df['Year'].min(), max_value=df['Year'].max(), value=(df['Year'].min(), df['Year'].max()))
+
+# Extracting the minimum and maximum years
+selected_min_year = elected_year[0]
+selected_max_year = elected_year[1]
+
+
 
 # Filter data based on selected years
-filtered_data = {year: sizes for year, sizes in data.items() if year >= selected_year[0] and year <= selected_year[1]}
+# Filter the DataFrame based on the selected years
+filtered_df = df[(df['Year'] >= selected_min_year) & (df['Year'] <= selected_max_year)]
 
-# Aggregate data across the selected years
-aggregated_data = {}
-for sizes in filtered_data.values():
-    for topic, size in sizes.items():
-        aggregated_data[topic] = aggregated_data.get(topic, 0) + size
 
-labels = list(aggregated_data.keys())
-sizes = list(aggregated_data.values())
+# Now you would aggregate the data as per your requirement, for example, if you're interested in the 'Category' column:
+aggregated_data = filtered_df['Category'].value_counts()
+
+#################################### DONUT CHART ####################################################
+
+
+st.markdown("<h1 style='text-align: center;'>Themes</h1>", unsafe_allow_html=True)
+
+st.markdown(f"<h3 style='text-align: center;'>{selected_min_year} to {selected_max_year}</h2>", unsafe_allow_html=True)
+# Display the selected values
+#st.write(f"You have selected the year range: {selected_min_year} to {selected_max_year}")
+
+# Now you can plot the donut chart with the aggregated data
+labels = aggregated_data.index
+sizes = aggregated_data.values
 
 # Pie chart settings
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'purple', 'orange']
-explode = (0, 0.1, 0, 0, 0, 0)  # explode the 2nd slice (Sexual Assault)
+#colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'purple', 'orange']
+colors = ['#1f77b4',  # muted blue
+          '#2ca02c',  # cooked asparagus green
+          '#17becf',  # blue-teal
+          '#98df8a',  # pale green
+          '#d62728',  # brick red
+          '#9467bd',  # muted purple
+          '#c5b0d5',  # pale purple
+          '#7f7f7f']  # middle gray
 
-plt.figure(figsize=(10, 6))
-plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+#explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)  # explode the 2nd slice (Sexual Assault)
+explode = [0] * len(labels)  # No slice is exploded, but can be customized as needed.
+
+# Set chart size
+plt.figure(figsize=(12, 8))
+wedges, texts, autotexts = plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140, wedgeprops=dict(width=0.25, edgecolor= 'black'))
+
+# Here's where you can set the size of the labels.
+plt.setp(texts, size=19) # Set the property 'size' for label texts to 12, or any other value you prefer
+plt.setp(autotexts, size=19) # Set the property 'size' for autotexts (the percentage labels) to 12 and make them bold
+
+# Adjust subplot parameters to move the pie chart
+plt.subplots_adjust(top=1.5, bottom=0.1)  # Adjust the bottom parameter to move the chart down
+
 plt.axis('equal')
 
-# Display the pie chart
+# Display the donut chart
+st.pyplot(plt.gcf())
+################################### RESULTS DONUT CHART #################################################################################
+
+st.markdown("<h1 style='text-align: center;'>Results of Recommendation</h1>", unsafe_allow_html=True)
+
+
+# Now count the occurrences of each result in the filtered data
+result_counts = filtered_df['Result '].value_counts()
+
+# Assuming 'result_counts' contains the counts of each result
+labels = result_counts.index
+sizes = result_counts.values
+colors = plt.cm.tab20c.colors  # Using a colormap for variety
+
+fig, ax = plt.subplots()
+#ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, wedgeprops=dict(width=0.4, edgecolor='w'))
+
+#explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)  # explode the 2nd slice (Sexual Assault)
+explode = [0] * len(labels)  # No slice is exploded, but can be customized as needed.
+
+# Set chart size
+plt.figure(figsize=(12, 8))
+wedges, texts, autotexts = plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140, wedgeprops=dict(width=0.25, edgecolor= 'black'))
+
+# Here's where you can set the size of the labels.
+plt.setp(texts, size=19) # Set the property 'size' for label texts to 12, or any other value you prefer
+plt.setp(autotexts, size=19) # Set the property 'size' for autotexts (the percentage labels) to 12 and make them bold
+
+# Adjust subplot parameters to move the pie chart
+plt.subplots_adjust(top=1.5, bottom=0.1)  # Adjust the bottom parameter to move the chart down
+
+plt.axis('equal')
+
+# Display the donut chart in Streamlit
 st.pyplot(plt)
 
+#################################### LINE CHART OF THEMES OVER THE YEARS #######################################################################
+
+st.markdown("<h1 style='text-align: center;'>Themes Over All Years</h1>", unsafe_allow_html=True)
+
+# Count the occurrences of each category per year
+category_counts = df.groupby(['Year', 'Category']).size().reset_index(name='Counts')
+
+# Pivot the results to get years as index and categories as columns
+category_counts_pivot = category_counts.pivot(index='Year', columns='Category', values='Counts').fillna(0)
+
+# Plotting
+# Plotting with a specified figure size (width, height)
+fig, ax = plt.subplots(figsize=(14, 6))  # Increase the width for a longer chart
+
+
+# Plot each category count as a line
+for category in category_counts_pivot.columns:
+    ax.plot(category_counts_pivot.index, category_counts_pivot[category], label=category)
+
+# Formatting
+#ax.figure(figsize=(12, 10))
+ax.set_xlabel('Year')
+ax.set_ylabel('Count')
+ax.legend()
+
+# Show the plot in Streamlit
+st.pyplot(fig)
--- pages/onthetable.py
+++ pages/onthetable.py
@@ -1,44 +1,98 @@
+
+
+
+
 import streamlit as st
 import pandas as pd
 
-# Sample data
-data = {
-    'ID': [1, 2, 3, 4, 5, 6, 7],
-    'Year': [1996, 2000, 2005, 2010, 2015, 2020, 2022],
-    'Short_Recommendation': ['Improve Security', 'Enhance Security', 'Boost Security', 'Upgrade Security', 'Strengthen Security', 'Improve Security Again', 'Strengthen Security Again'],
-    'Full_Recommendation': ['Full text for Improve Security', 'Full text for Enhance Security', 'Full text for Boost Security', 'Full text for Upgrade Security', 'Full text for Strengthen Security', 'Full text for Improve Security Again', 'Full text for Strengthen Security Again'],
-    'Similar_to': ['None', '1', '2', '3', '4', '5', '6'],
-    'Status': ['Not Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred'],
-    'Category': ['Security', 'Security', 'Security', 'IT', 'IT', 'HR', 'HR']
+
+
+#df = pd.read_excel('/workspaces/VA-Recommendation-Analysis-App/ACWV Recommendations with Categories and Similarity Groups.xlsx')
+
+# Display the DataFrame
+#data = load_data()
+#st.table(df)
+
+
+
+##################################################### BANNER  #####################################################################
+
+st.markdown("""
+<style>
+.banner {
+  background-color: black;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
 }
-df = pd.DataFrame(data)
+</style>
 
-st.title('Recommendations on the Table')
+<div class="banner">
+  <h1 style="color:white;">Reoccuring Recommendations</h1>
+</div>
+""", unsafe_allow_html=True)
 
-# Create a category filter with counts
-categories = df['Category'].unique()
-category_counts = df[df['Status'] == 'Not Concurred'].groupby('Category').size()
-category_options = [f"{category} ({category_counts.get(category, 0)})" for category in categories]
+######################################### DISPLAY SIMILAR RECOMMENDATIONS DF #######################################################
 
-# Use multiselect instead of dropdown
-selected_categories = st.sidebar.multiselect('Filter by Category:', category_options)
-selected_categories = [cat.split(' ')[0] for cat in selected_categories]
+def load_data():
+    data = pd.read_excel('/workspaces/VA-Recommendation-Analysis-App/ACWV Recommendations with Categories and Similarity Groups.xlsx')
 
-# Filter the DataFrame based on the status 'Not Concurred' and selected categories
-if selected_categories:
-    df_filtered = df[(df['Status'] == 'Not Concurred') & (df['Category'].isin(selected_categories))]
-else:
-    df_filtered = df[df['Status'] == 'Not Concurred']
+    # Select a subset of columns if the DataFrame is very wide
+    columns_to_display = ['ChainID', 'Year', 'ID', 'Category', 'Recommendation']
+    data = data[columns_to_display]
+    
 
-# Loop through filtered DataFrame and display interactive elements
-for index, row in df_filtered.iterrows():
-    button_label = f"Recommendation {row['ID']} from {row['Year']} (Status: {row['Status']})"
-    if st.button(button_label):
-        st.write(f"**Full Recommendation**: {row['Full_Recommendation']}")
+    # Filter out rows with nan chain ID
+    data = data.dropna(subset=['ChainID'])
+
+    # Sort the DataFrame by the chain ID in descending order
+    data = data.sort_values(by='ChainID', ascending=True)
+
+    return data
+data = load_data()
+st.table(data)
+
+
+############################################## OLD STUFF ###########################################################################
+
+# Sample data
+# data = {
+#     'ID': [1, 2, 3, 4, 5, 6, 7],
+#     'Year': [1996, 2000, 2005, 2010, 2015, 2020, 2022],
+#     'Short_Recommendation': ['Improve Security', 'Enhance Security', 'Boost Security', 'Upgrade Security', 'Strengthen Security', 'Improve Security Again', 'Strengthen Security Again'],
+#     'Full_Recommendation': ['Full text for Improve Security', 'Full text for Enhance Security', 'Full text for Boost Security', 'Full text for Upgrade Security', 'Full text for Strengthen Security', 'Full text for Improve Security Again', 'Full text for Strengthen Security Again'],
+#     'Similar_to': ['None', '1', '2', '3', '4', '5', '6'],
+#     'Status': ['Not Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred', 'Not Concurred', 'Concurred'],
+#     'Category': ['Security', 'Security', 'Security', 'IT', 'IT', 'HR', 'HR']
+# }
+# df = pd.DataFrame(data)
+
+# st.title('Recommendations on the Table')
+
+# # Create a category filter with counts
+# categories = df['Category'].unique()
+# category_counts = df[df['Status'] == 'Not Concurred'].groupby('Category').size()
+# category_options = [f"{category} ({category_counts.get(category, 0)})" for category in categories]
+
+# # Use multiselect instead of dropdown
+# selected_categories = st.sidebar.multiselect('Filter by Category:', category_options)
+# selected_categories = [cat.split(' ')[0] for cat in selected_categories]
+
+# # Filter the DataFrame based on the status 'Not Concurred' and selected categories
+# if selected_categories:
+#     df_filtered = df[(df['Status'] == 'Not Concurred') & (df['Category'].isin(selected_categories))]
+# else:
+#     df_filtered = df[df['Status'] == 'Not Concurred']
+
+# # Loop through filtered DataFrame and display interactive elements
+# for index, row in df_filtered.iterrows():
+#     button_label = f"Recommendation {row['ID']} from {row['Year']} (Status: {row['Status']})"
+#     if st.button(button_label):
+#         st.write(f"**Full Recommendation**: {row['Full_Recommendation']}")
         
-        similar_to_ids = row['Similar_to'].split(',')
-        similar_recs = df[df['ID'].astype(str).isin(similar_to_ids)]
+#         similar_to_ids = row['Similar_to'].split(',')
+#         similar_recs = df[df['ID'].astype(str).isin(similar_to_ids)]
         
-        st.write("**Similar to the following recommendations:**")
-        for sim_index, sim_row in similar_recs.iterrows():
-            st.write(f"Recommendation {sim_row['ID']} from {sim_row['Year']}: {sim_row['Full_Recommendation']}")
+#         st.write("**Similar to the following recommendations:**")
+#         for sim_index, sim_row in similar_recs.iterrows():
+#             st.write(f"Recommendation {sim_row['ID']} from {sim_row['Year']}: {sim_row['Full_Recommendation']}")
